2c98b56bf0ea4f3ffada2dcf0d5e6e34479f2519
Marcelo Henrique Machado <marcelo.henrique.machado@everis.com>
2019-03-06 11:47:03 -0300

    inicial

---
+++ src/main/java/com/marcelo/clientes/CadastroClientesApplication.java
@@ -0,0 +1,13 @@
+package com.marcelo.clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroClientesApplication.class, args);
+	}
+
+}
